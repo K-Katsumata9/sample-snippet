c180935dac0efbbd30dfd52ec392f1bf84e92829
K-Katsumata9 <dowbler@icloud.com>
2024-05-18 23:09:02 +0900

    add test data

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -7,3 +7,8 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+
+20.times do |i|
+	Snippet.create(title:"#{i+1}個目のタイトル",language:"ruby",contents:"#{i}個目のタイトル")
+end
